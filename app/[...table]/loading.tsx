f45c9dd5bde625398c8c913c81876dc31b26334b
Denys Kravchenko <denys.montreal@gmail.com>
2024-11-22 10:44:45 -0800

    Improve loading component styling and refactor Week component class handling for better readability

--- app/[...table]/loading.tsx
+++ app/[...table]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const LoadingPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen m-auto">
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
     </div>
   );
--- components/Week.tsx
+++ components/Week.tsx
@@ -1,14 +1,16 @@
 "use client";
-import React from "react";
 import { WeekProps } from "../lib/types";
 
 // Week component to display a single week with conditional styling
 const Week = ({ weekIndex, isFilled, yearsAlive }: WeekProps) => {
   return (
     <div
-      className={`size-2 border border-black dark:border-purple-500 after:content-[attr(title)] relative after:flex after:text-xs after:absolute after:left-full after:ms-4 after:leading-2 after:w-20 after:top-0 dark:text-purple-500 ${
-        isFilled ? "bg-black dark:bg-purple-500" : ""
-      } ${weekIndex > 26 ? "ml-auto" : ""}`}
+      className={[
+        `after:content-[attr(title)] after:absolute after:left-full after:ms-4 after:leading-2 after:w-20 after:top-0 after:flex after:text-xs`,
+        `size-2 border border-black dark:border-purple-500  relative dark:text-purple-500`,
+        isFilled ? "bg-black dark:bg-purple-500" : "",
+        weekIndex > 26 ? "ml-auto" : "",
+      ].join(" ")}
       // title={`${yearsAlive}`}
       {...(yearsAlive ? { title: `${yearsAlive}` } : {})}
     ></div>
