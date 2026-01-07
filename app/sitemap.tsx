02aa67627550e026a0480754c35c04b40e3d2e3d
denis.montreal <71228-denis.montreal@users.noreply.drupalcode.org>
2026-01-06 16:08:41 -0800

    fix: Correct URL format in sitemap for the about page

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -18,7 +18,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}about`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
